b7fb33db0650121e6875fa7ede6a75027716903f
muhsinmuhsy <muhsinkoodali856@gmail.com>
2023-07-25 16:32:01 +0530

    salary added

--- Admin_App/admin.py
+++ Admin_App/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from Admin_App.models import *
 # Register your models here.
 @admin.register(Customer)
-class CustomersModelAdmin(admin.ModelAdmin):
+class CustomerModelAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Customer._meta.fields]
 
 
 @admin.register(Order)
-class OrdersModelAdmin(admin.ModelAdmin):
+class OrderModelAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Order._meta.fields]
+
+
+@admin.register(DeliveryBoySalary)
+class DeliveryBoySalaryModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in DeliveryBoySalary._meta.fields]
